6cbc62ff35151be6cd71ab349e3552c3670df0fb
Bruno Alves <119462960+BruDevs@users.noreply.github.com>
2023-03-01 22:38:37 -0300

    Add files via upload

---
+++ Add de produtos com Foreach/For Reach/For Reach/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿
+namespace For_Reach
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.txtProduto = new System.Windows.Forms.TextBox();
+            this.txtVunitario = new System.Windows.Forms.TextBox();
+            this.txtQtd = new System.Windows.Forms.TextBox();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Produto = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Valor_unitario = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.QTD = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Total = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtProduto
+            // 
+            this.txtProduto.Location = new System.Drawing.Point(81, 160);
+            this.txtProduto.Name = "txtProduto";
+            this.txtProduto.Size = new System.Drawing.Size(152, 20);
+            this.txtProduto.TabIndex = 0;
+            // 
+            // txtVunitario
+            // 
+            this.txtVunitario.Location = new System.Drawing.Point(81, 243);
+            this.txtVunitario.Name = "txtVunitario";
+            this.txtVunitario.Size = new System.Drawing.Size(152, 20);
+            this.txtVunitario.TabIndex = 1;
+            // 
+            // txtQtd
+            // 
+            this.txtQtd.Location = new System.Drawing.Point(81, 326);
+            this.txtQtd.Name = "txtQtd";
+            this.txtQtd.Size = new System.Drawing.Size(152, 20);
+            this.txtQtd.TabIndex = 2;
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Location = new System.Drawing.Point(582, 362);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.Size = new System.Drawing.Size(100, 20);
+            this.txtTotal.TabIndex = 3;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Produto,
+            this.Valor_unitario,
+            this.QTD,
+            this.Total});
+            this.dataGridView1.Location = new System.Drawing.Point(239, 160);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(443, 186);
+            this.dataGridView1.TabIndex = 4;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Lucida Console", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(79, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 11);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Produto:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Lucida Console", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(79, 229);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 11);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Valor unitário:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Lucida Console", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(79, 311);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 11);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Quantidade:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Lucida Console", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(529, 366);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 11);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Total:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(239, 359);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Adicionar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Lucida Console", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(200, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(415, 34);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Cadastro de produtos";
+            // 
+            // Produto
+            // 
+            dataGridViewCellStyle1.NullValue = null;
+            this.Produto.DefaultCellStyle = dataGridViewCellStyle1;
+            this.Produto.HeaderText = "Produto";
+            this.Produto.Name = "Produto";
+            // 
+            // Valor_unitario
+            // 
+            dataGridViewCellStyle2.Format = "C2";
+            dataGridViewCellStyle2.NullValue = null;
+            this.Valor_unitario.DefaultCellStyle = dataGridViewCellStyle2;
+            this.Valor_unitario.HeaderText = "V. Unitário";
+            this.Valor_unitario.Name = "Valor_unitario";
+            // 
+            // QTD
+            // 
+            this.QTD.HeaderText = "QTD";
+            this.QTD.Name = "QTD";
+            // 
+            // Total
+            // 
+            dataGridViewCellStyle3.Format = "C2";
+            dataGridViewCellStyle3.NullValue = null;
+            this.Total.DefaultCellStyle = dataGridViewCellStyle3;
+            this.Total.HeaderText = "Total";
+            this.Total.Name = "Total";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 398);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.txtQtd);
+            this.Controls.Add(this.txtVunitario);
+            this.Controls.Add(this.txtProduto);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtProduto;
+        private System.Windows.Forms.TextBox txtVunitario;
+        private System.Windows.Forms.TextBox txtQtd;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Produto;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Valor_unitario;
+        private System.Windows.Forms.DataGridViewTextBoxColumn QTD;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Total;
+    }
+}
+
---
+++ Add de produtos com Foreach/For Reach/For Reach/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace For_Reach
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double vUnit, vTotal, total = 0;
+            int qtd;
+            string nome;
+
+            nome = txtProduto.Text;
+            qtd = Convert.ToInt32(txtQtd.Text);
+            vUnit = Convert.ToDouble(txtVunitario.Text);
+            vTotal = qtd * vUnit;
+
+            dataGridView1.Rows.Add(nome, vUnit, qtd, vTotal);
+
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                total += Convert.ToDouble(row.Cells["Total"].Value);
+            }
+
+            txtTotal.Text = Convert.ToDouble(total).ToString("C");
+
+            limpar();
+           
+
+        }
+
+        private void limpar() 
+        {
+            txtProduto.Clear();
+            txtQtd.Clear();
+            txtVunitario.Clear();
+        }
+    }
+}
